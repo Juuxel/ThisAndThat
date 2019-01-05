1895a795601d21497863c346578ed9d909cfedad
Juuxel <kasperi.kauppi@gmail.com>
2019-01-05 22:10:02 +0200

    Add missing license headers

--- src/main/java/juuxel/thisandthat/mixin/BurnTimeMixin.java
+++ src/main/java/juuxel/thisandthat/mixin/BurnTimeMixin.java
@@ -1,3 +1,7 @@
+/* This file is a part of the This & That project
+ * by Juuxel, licensed under the MIT license.
+ * Full code and license: https://github.com/Juuxel/ThisAndThat
+ */
 package juuxel.thisandthat.mixin;
 
 import juuxel.thisandthat.lib.ModItems;
--- src/main/kotlin/juuxel/thisandthat/util/MultipartUtils.kt
+++ src/main/kotlin/juuxel/thisandthat/util/MultipartUtils.kt
@@ -1,3 +1,7 @@
+/* This file is a part of the This & That project
+ * by Juuxel, licensed under the MIT license.
+ * Full code and license: https://github.com/Juuxel/ThisAndThat
+ */
 package juuxel.thisandthat.util
 
 import net.minecraft.block.enums.BlockHalf

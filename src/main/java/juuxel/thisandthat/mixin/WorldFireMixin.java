1962a6994c2ad1dc4024e79929e2f9a9741afa02
Juuxel <kasperi.kauppi@gmail.com>
2019-01-06 14:02:09 +0200

    Remove unnecessary implementation of ViewableWorld

--- src/main/java/juuxel/thisandthat/mixin/WorldFireMixin.java
+++ src/main/java/juuxel/thisandthat/mixin/WorldFireMixin.java
@@ -8,14 +8,13 @@ import juuxel.thisandthat.lib.ModTags;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
-import net.minecraft.world.ViewableWorld;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(World.class)
-public abstract class WorldFireMixin implements ViewableWorld {
+public abstract class WorldFireMixin {
     @Redirect(method = "doesAreaContainFireSource", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;getBlock()Lnet/minecraft/block/Block;"))
     private Block getBlockProxy(BlockState state) {
         if (state.getBlock().matches(ModTags.INSTANCE.getFire()))
